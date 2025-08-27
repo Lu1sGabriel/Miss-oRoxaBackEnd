91c38999c19e3c89503bea77f7f1257e472b61fe
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-08-27 13:20:23 -0300

    feat: Início do projeto

---
+++ src/main/java/org/missao/roxa/missaoroxabackend/MissaoRoxaBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.missao.roxa.missaoroxabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissaoRoxaBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MissaoRoxaBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/missao/roxa/missaoroxabackend/MissaoRoxaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.missao.roxa.missaoroxabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissaoRoxaBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
