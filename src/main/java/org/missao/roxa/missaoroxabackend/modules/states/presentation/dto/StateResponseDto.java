5b7c5e32248af22a2372eeb9f9fe4e048e1c05ed
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-09-09 20:22:15 -0300

    Feat: Criação da classe record `StateResponseDto`

---
+++ src/main/java/org/missao/roxa/missaoroxabackend/modules/states/presentation/dto/StateResponseDto.java
@@ -0,0 +1,17 @@
+package org.missao.roxa.missaoroxabackend.modules.states.presentation.dto;
+
+import org.missao.roxa.missaoroxabackend.core.shared.helper.mapper.IMappableDto;
+
+import java.util.List;
+import java.util.UUID;
+
+public record StateResponseDto(
+        UUID id,
+        String name,
+        List<Municipality> municipality
+) implements IMappableDto {
+
+    public record Municipality(UUID id, String name) {
+    }
+
+}
