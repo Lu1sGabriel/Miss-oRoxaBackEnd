7b1940f1b51ed7e69012817e1d1e700e28e0de68
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-09-09 19:58:09 -0300

    Feat: Adição do cache e configuração da serialização da pagination

--- src/main/java/org/missao/roxa/missaoroxabackend/MissaoRoxaBackEndApplication.java
+++ src/main/java/org/missao/roxa/missaoroxabackend/MissaoRoxaBackEndApplication.java
@@ -2,8 +2,12 @@ package org.missao.roxa.missaoroxabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @SpringBootApplication
+@EnableCaching
 public class MissaoRoxaBackEndApplication {
 
     public static void main(String[] args) {
