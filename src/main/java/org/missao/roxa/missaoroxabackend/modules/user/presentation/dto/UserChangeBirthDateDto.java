e608e8f56337777358cc0f8d0dc9629564eb6cf8
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-09-09 20:15:34 -0300

    Feat: Criação do `UserChangeBirthDateDto` e `UserChangeFullNameDto`

---
+++ src/main/java/org/missao/roxa/missaoroxabackend/modules/user/presentation/dto/UserChangeBirthDateDto.java
@@ -0,0 +1,6 @@
+package org.missao.roxa.missaoroxabackend.modules.user.presentation.dto;
+
+import java.time.LocalDate;
+
+public record UserChangeBirthDateDto(LocalDate birthDate) {
+}
---
+++ src/main/java/org/missao/roxa/missaoroxabackend/modules/user/presentation/dto/UserChangeFullNameDto.java
@@ -0,0 +1,4 @@
+package org.missao.roxa.missaoroxabackend.modules.user.presentation.dto;
+
+public record UserChangeFullNameDto(String fullName) {
+}
