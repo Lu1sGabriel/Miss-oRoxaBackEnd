98a44ae665d086e492d98753c4168c478268f09d
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-09-09 19:20:11 -0300

    Refactor: Remoção de métodos não usados em `AccountRepository`

--- src/main/java/org/missao/roxa/missaoroxabackend/modules/account/infrastructure/repository/AccountRepository.java
+++ src/main/java/org/missao/roxa/missaoroxabackend/modules/account/infrastructure/repository/AccountRepository.java
@@ -1,19 +1,15 @@
 package org.missao.roxa.missaoroxabackend.modules.account.infrastructure.repository;
 
 import org.missao.roxa.missaoroxabackend.modules.account.domain.AccountEntity;
-import org.missao.roxa.missaoroxabackend.modules.user.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-@org.springframework.stereotype.Repository
+@Repository
 public interface AccountRepository extends JpaRepository<AccountEntity, UUID> {
-
-    Optional<AccountEntity> findByUser(UserEntity user);
-
     Optional<AccountEntity> findByCredentials_Email_Email(String email);
 
     Optional<AccountEntity> findByCredentials_PhoneNumber_Number(String phoneNumber);
-
 }
