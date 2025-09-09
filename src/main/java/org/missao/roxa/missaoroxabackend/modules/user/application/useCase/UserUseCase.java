81e4247ecb4cae465892b82fea7311c64178f909
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-09-09 19:58:45 -0300

    Feat: Adição de mais `useCase`

--- src/main/java/org/missao/roxa/missaoroxabackend/modules/user/application/useCase/UserUseCase.java
+++ src/main/java/org/missao/roxa/missaoroxabackend/modules/user/application/useCase/UserUseCase.java
@@ -1,11 +1,16 @@
 package org.missao.roxa.missaoroxabackend.modules.user.application.useCase;
 
+import org.missao.roxa.missaoroxabackend.modules.user.application.useCase.changeBirthDate.IUserChangeBirthDate;
+import org.missao.roxa.missaoroxabackend.modules.user.application.useCase.changeFullName.IUserChangeFullName;
 import org.missao.roxa.missaoroxabackend.modules.user.application.useCase.create.IUserCreate;
+import org.missao.roxa.missaoroxabackend.modules.user.application.useCase.find.IUserFind;
 import org.springframework.stereotype.Component;
 
 @Component
 public record UserUseCase(
-        IUserCreate getCreate
+        IUserFind find,
+        IUserCreate getCreate,
+        IUserChangeFullName changeFullName,
+        IUserChangeBirthDate changeBirthDate
 ) {
-
 }
