ae3c7452a9329597fe2744df729d933366f14a1d
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-09-09 19:25:39 -0300

    Refactor: Remoção e criação de novos métodos

--- src/main/java/org/missao/roxa/missaoroxabackend/modules/address/infrastructure/repository/AddressRepository.java
+++ src/main/java/org/missao/roxa/missaoroxabackend/modules/address/infrastructure/repository/AddressRepository.java
@@ -1,6 +1,10 @@
 package org.missao.roxa.missaoroxabackend.modules.address.infrastructure.repository;
 
 import org.missao.roxa.missaoroxabackend.modules.address.domain.AddressEntity;
+import org.missao.roxa.missaoroxabackend.modules.municipality.domain.MunicipalityEntity;
+import org.missao.roxa.missaoroxabackend.modules.states.domain.StateEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +12,10 @@ import java.util.UUID;
 
 @Repository
 public interface AddressRepository extends JpaRepository<AddressEntity, UUID> {
+    Page<AddressEntity> findByPostalCode_PostalCode(String postalCode, Pageable pageable);
+
+    Page<AddressEntity> findByMunicipality(MunicipalityEntity municipality, Pageable pageable);
+
+    Page<AddressEntity> findByMunicipality_State(StateEntity stateEntity, Pageable pageable);
+
 }
