e0a5b3f1d2a104bb071dc29c7af70f80549eec5a
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-09-09 19:25:12 -0300

    Refactor: Agora quando se cria um `Address` faz o set em user: `user.setAddress()`

--- src/main/java/org/missao/roxa/missaoroxabackend/modules/address/domain/factory/AddressFactory.java
+++ src/main/java/org/missao/roxa/missaoroxabackend/modules/address/domain/factory/AddressFactory.java
@@ -1,8 +1,12 @@
 package org.missao.roxa.missaoroxabackend.modules.address.domain.factory;
 
 import org.missao.roxa.missaoroxabackend.modules.address.domain.AddressEntity;
-import org.missao.roxa.missaoroxabackend.modules.address.domain.value.*;
-import org.missao.roxa.missaoroxabackend.modules.address.dto.AddressCreateDto;
+import org.missao.roxa.missaoroxabackend.modules.address.domain.value.Complement;
+import org.missao.roxa.missaoroxabackend.modules.address.domain.value.Neighborhood;
+import org.missao.roxa.missaoroxabackend.modules.address.domain.value.PostalCode;
+import org.missao.roxa.missaoroxabackend.modules.address.domain.value.Street;
+import org.missao.roxa.missaoroxabackend.modules.address.presentation.dto.AddressCreateDto;
+import org.missao.roxa.missaoroxabackend.modules.municipality.domain.MunicipalityEntity;
 import org.missao.roxa.missaoroxabackend.modules.user.domain.UserEntity;
 
 public final class AddressFactory {
@@ -10,16 +14,17 @@ public final class AddressFactory {
     private AddressFactory() {
     }
 
-    public static AddressEntity create(UserEntity user, AddressCreateDto dto) {
-        return new AddressEntity(
+    public static AddressEntity create(UserEntity user, MunicipalityEntity municipality, AddressCreateDto dto) {
+        var address = new AddressEntity(
                 user,
+                municipality,
                 new PostalCode(dto.postalCode()),
                 new Street(dto.street()),
                 new Complement(dto.complement().orElse(null)),
-                new Neighborhood(dto.neighborhood()),
-                new City(dto.city()),
-                new State(dto.state())
+                new Neighborhood(dto.neighborhood())
         );
+        user.setAddress(address);
+        return address;
     }
 
 }
