e7e66138f2bc79e2d6757eb7cfb43ccd44ed694e
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-09-09 19:58:34 -0300

    Refactor: Remoção do campo `createdAt` no DTO

--- src/main/java/org/missao/roxa/missaoroxabackend/modules/user/presentation/dto/UserResponseDto.java
+++ src/main/java/org/missao/roxa/missaoroxabackend/modules/user/presentation/dto/UserResponseDto.java
@@ -1,10 +1,9 @@
 package org.missao.roxa.missaoroxabackend.modules.user.presentation.dto;
 
 import org.missao.roxa.missaoroxabackend.core.shared.helper.mapper.IMappableDto;
-import org.missao.roxa.missaoroxabackend.modules.account.dto.AccountResponseDto;
-import org.missao.roxa.missaoroxabackend.modules.address.dto.AddressResponseDto;
+import org.missao.roxa.missaoroxabackend.modules.account.presentation.dto.AccountResponseDto;
+import org.missao.roxa.missaoroxabackend.modules.address.presentation.dto.AddressResponseDto;
 
-import java.time.Instant;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -12,7 +11,6 @@ public record UserResponseDto(
         UUID id,
         String fullName,
         LocalDate birthDate,
-        Instant createdAt,
         AccountResponseDto account,
         AddressResponseDto address
 ) implements IMappableDto {
