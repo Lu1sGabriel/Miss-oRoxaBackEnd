cfe60ff6498349ff928a5ca2a4fb5d27cba3e610
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-09-11 18:25:50 -0300

    Feat: Adição do `useCase` `IAccountChangePassword` na classe que engloba todos os `useCases` de `Account`

--- src/main/java/org/missao/roxa/missaoroxabackend/modules/account/application/useCase/AccountUseCase.java
+++ src/main/java/org/missao/roxa/missaoroxabackend/modules/account/application/useCase/AccountUseCase.java
@@ -1,12 +1,14 @@
 package org.missao.roxa.missaoroxabackend.modules.account.application.useCase;
 
 import org.missao.roxa.missaoroxabackend.modules.account.application.useCase.changeEmail.IAccountChangeEmail;
+import org.missao.roxa.missaoroxabackend.modules.account.application.useCase.changePassword.IAccountChangePassword;
 import org.missao.roxa.missaoroxabackend.modules.account.application.useCase.changePhoneNumber.IAccountChangePhoneNumber;
 import org.springframework.stereotype.Component;
 
 @Component
 public record AccountUseCase(
         IAccountChangeEmail changeEmail,
-        IAccountChangePhoneNumber changePhoneNumber
+        IAccountChangePhoneNumber changePhoneNumber,
+        IAccountChangePassword changePassword
 ) {
 }
